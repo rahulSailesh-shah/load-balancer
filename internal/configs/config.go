316cbd6927d98f7628e1ff4bf747c761bd588e07
rahulSailesh-shah <shah.rahulsailesh@gmail.com>
2025-04-26 21:19:01 -0700

    YAML config parser

---
+++ internal/configs/config.go
@@ -0,0 +1,46 @@
+package configs
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+type resource struct {
+	Name            string
+	Endpoint        string
+	Destination_URL string
+}
+
+type server struct {
+	Host string
+	Port string
+}
+
+type configuration struct {
+	Server    server
+	Resources []resource
+}
+
+var Config *configuration
+
+func NewConfiguration() (*configuration, error) {
+	viper.AddConfigPath("data")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AutomaticEnv()
+	viper.SetEnvKeyReplacer(strings.NewReplacer(`.`, `_`))
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		return nil, fmt.Errorf("error loading config file: %s", err)
+	}
+
+	err = viper.Unmarshal(&Config)
+	if err != nil {
+		return nil, fmt.Errorf("error reading config file: %s", err)
+	}
+
+	return Config, nil
+}
